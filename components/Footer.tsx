5fb5a0e5512b9cbed2977a2504a5b27398048177
YuvanX <abhivigneshofficial@gmail.com>
2025-01-30 16:57:03 +0530

    footer email fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return<div className="w-full font-space flex flex-col md:flex-row  md:justify-between px-10 lg:px-20 text-lg md:text-xl">
         <div>
-            <div className="text-neutral-400 hover:text-neutral-100 hover:underline cursor-pointer">abhivignesh@gmail.com</div>
+            <div className="text-neutral-400 hover:text-neutral-100 hover:underline cursor-pointer">abhivigneshofficial@gmail.com</div>
         </div>
         <div>
             <div className="text-neutral-400 hover:text-neutral-100 hover:underline cursor-pointer">Contact&nbsp;: &nbsp;+91 9059885500</div>
